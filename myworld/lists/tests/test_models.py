4c40b7c7117ed925ac3fae30facbe836b36933a7
fb0801 <fbhatti0801@gmail.com>
2023-11-08 17:46:52 +0000

    chapter 13.1.1 A Django Quirk: Model Save Doesn’t Run Validation

--- myworld/lists/tests/test_models.py
+++ myworld/lists/tests/test_models.py
@@ -31,3 +31,4 @@ class ListAndItemModelsTest(TestCase):
         item = Item(list=list_, text='')
         with self.assertRaises(ValidationError):
             item.save()
+            item.full_clean()
