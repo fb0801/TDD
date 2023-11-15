03a36a1b2b6065425e652a2657ca92aaf36972e5
fb0801 <fbhatti0801@gmail.com>
2023-11-15 18:05:26 +0000

    Refactor list view to handle new item POSTs

--- myworld/lists/views.py
+++ myworld/lists/views.py
@@ -15,10 +15,7 @@ def view_list(request, list_id):
         return redirect(f'/lists/{list_.id}/')
     return render(request, 'list.html', {'list': list_})
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+
 
 
 def new_list(request):
