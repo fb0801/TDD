3eaef6a18aed39918c8c2de792c7c1e987559a8d
fb0801 <fbhatti0801@gmail.com>
2023-11-22 18:06:46 +0000

    Testing and Customising Form Validation

--- myworld/lists/forms.py
+++ myworld/lists/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from lists.models import Item
 
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
 class ItemForm(forms.models.ModelForm):
     item_text = forms.CharField(
         widget=forms.fields.TextInput(attrs={
@@ -18,4 +20,7 @@ class ItemForm(forms.models.ModelForm):
                 'placeholder': 'Enter a to-do item',
                 'class': 'form-control input-lg',
             }),
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
         }
