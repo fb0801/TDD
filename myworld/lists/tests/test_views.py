e65599f9144307ee48ee7e5a2afa342392affa83
fb0801 <fbhatti0801@gmail.com>
2023-11-26 16:50:12 +0000

    Using the Form in a View That Takes POST Requests

--- myworld/lists/tests/test_views.py
+++ myworld/lists/tests/test_views.py
@@ -72,7 +72,7 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         expected_error = escape("You can't have an empty list item")
