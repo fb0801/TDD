5d586d86ba9057d21708f363ad1480a6d4f0a9b0
fb0801 <fbhatti0801@gmail.com>
2023-12-23 17:11:48 +0000

    Some Integrity Errors Do Show Up on Save

--- myworld/lists/tests/test_models.py
+++ myworld/lists/tests/test_models.py
@@ -58,6 +58,7 @@ class ListModelsTest(TestCase):
         with self.assertRaises(ValidationError):
             item = Item(list=list_, text='bla')
             item.full_clean()
+            #item.save()
 
     def test_CAN_save_same_item_to_different_lists(self):
         list1 = List.objects.create()
