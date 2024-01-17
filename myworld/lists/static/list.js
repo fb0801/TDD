5511cd58b66410aa0be0485283781ac75df622bf
fb0801 <fbhatti0801@gmail.com>
2024-01-17 16:55:32 +0000

    Fixtures, Execution Order, and Global State: Key Challenges of JS Testing

--- myworld/lists/static/list.js
+++ myworld/lists/static/list.js
@@ -1,5 +1,6 @@
 
-
-$('input[name="text"]').on('keypress', function () { 
-    $('.has-error').hide();
-  });
+$('input[name="text"]').on('keypress', function () {
+  console.log('in keypress handler');
+  $('.has-error').hide();
+});
+console.log('list.js loaded');
