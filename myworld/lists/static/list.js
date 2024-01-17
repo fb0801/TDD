f616984ea7d422b73756fd63f3ceaab4e4eb6bda
fb0801 <fbhatti0801@gmail.com>
2024-01-17 17:23:55 +0000

    Using an Initialize Function for More Control Over Execution Time 2

--- myworld/lists/static/list.js
+++ myworld/lists/static/list.js
@@ -1,10 +1,6 @@
-
 var initialize = function () {
-  console.log('initialize called');
   $('input[name="text"]').on('keypress', function () {
-    console.log('in keypress handler');
     $('.has-error').hide();
   });
 };
-console.log('list.js loaded');
 
