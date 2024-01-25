66366f5fb2b5a1cbbf5ef8615cc4095769704b87
fb0801 <fbhatti0801@gmail.com>
2024-01-25 16:11:42 +0000

    Custom Authentication Models 2

--- myworld/accounts/models.py
+++ myworld/accounts/models.py
@@ -16,3 +16,20 @@ class ListUserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         self.create_user(email)
+        
+
+class ListUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(primary_key=True)
+    USERNAME_FIELD = 'email'
+    #REQUIRED_FIELDS = ['email', 'height']
+
+    objects = ListUserManager()
+
+    @property
+    def is_staff(self):
+        return self.email == 'harry.percival@example.com'
+
+    @property
+    def is_active(self):
+        return True
+
