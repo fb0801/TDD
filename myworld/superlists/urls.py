b26ab3bd515635a14b1759307c878572b4d655f7
fb0801 <fbhatti0801@gmail.com>
2024-01-25 15:41:15 +0000

    Sending Emails from Django

--- myworld/superlists/urls.py
+++ myworld/superlists/urls.py
@@ -16,16 +16,21 @@ Including another URLconf
 """
 from django.urls import re_path
 
-from django.conf.urls import include
+from django.conf.urls import include,url
 from lists import views as list_views  
 from lists import urls as list_urls  
+
+
+from accounts import urls as accounts_urls
 
 
 urlpatterns = [
     re_path(r'^$', list_views.home_page, name='home'),
     re_path(r'^lists/', include(list_urls)),
+    re_path(r'^accounts/', include(accounts_urls)),
     
 ]
+
 
 
 
