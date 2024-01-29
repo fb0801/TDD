3fa5123983d0d66cae3682e53c6619380869261a
fb0801 <fbhatti0801@gmail.com>
2024-01-29 16:00:34 +0000

    Using unittest.patch

--- myworld/accounts/views.py
+++ myworld/accounts/views.py
@@ -2,5 +2,12 @@ from django.core.mail import send_mail
 from django.shortcuts import redirect
 
 def send_login_email(request):
-    send_mail()
+    email = request.POST['email']
+    print(type(send_mail))
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email]
+    )
     return redirect('/')
