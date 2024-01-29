adf297b4a0f21c744ac367d3804ea4fff8762c3b
fb0801 <fbhatti0801@gmail.com>
2024-01-29 17:00:53 +0000

    Testing the Django Messages Framework 2

--- myworld/accounts/views.py
+++ myworld/accounts/views.py
@@ -5,10 +5,8 @@ from django.contrib import messages
 def send_login_email(request):
     email = request.POST['email']
     print(type(send_mail))
-    send_mail(
-        'Your login link for Superlists',
-        'body text tbc',
-        'noreply@superlists',
-        [email]
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
