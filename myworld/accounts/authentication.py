52040c104cc66d77eb98eefbc7ed262aa7bfffa1
fb0801 <fbhatti0801@gmail.com>
2024-02-06 17:10:41 +0000

    1 if = 1 More Test

--- myworld/accounts/authentication.py
+++ myworld/accounts/authentication.py
@@ -1,23 +1,12 @@
 import sys
-from accounts.models import ListUser, Token
+from accounts.models import User, Token
+
 
 class PasswordlessAuthenticationBackend(object):
 
     def authenticate(self, uid):
-        print('uid', uid, file=sys.stderr)
-        if not Token.objects.filter(uid=uid).exists():
-            print('no token found', file=sys.stderr)
-            return None
         token = Token.objects.get(uid=uid)
-        print('got token', file=sys.stderr)
-        try:
-            user = ListUser.objects.get(email=token.email)
-            print('got user', file=sys.stderr)
-            return user
-        except ListUser.DoesNotExist:
-            print('new user', file=sys.stderr)
-            return ListUser.objects.create(email=token.email)
+        return User.objects.get(email=token.email)
 
 
-    def get_user(self, email):
-        return ListUser.objects.get(email=email)
+    
