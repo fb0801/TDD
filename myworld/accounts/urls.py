5dc0d8fc8d011cc6c0254085916756bc8e6cbf7b
fb0801 <fbhatti0801@gmail.com>
2024-02-12 16:04:33 +0000

    The Moment of Truth: Will the FT Pass 4 and logout

--- myworld/accounts/urls.py
+++ myworld/accounts/urls.py
@@ -1,14 +1,13 @@
 #from django.conf.urls import url
 from accounts import views
 from django.urls import re_path
+from django.contrib.auth.views import logout
+
+
 
 urlpatterns = [
-    
+    re_path(r'^send_login_email$', views.send_login_email, name='send_login_email'),
+    re_path(r'^login$', views.login, name='login'),
+    re_path(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
 
-'''
-urlpatterns = [
-    re_path(r'^send_email$', views.send_login_email, name='send_login_email'),
-    re_path(r'^login$', views.login, name='login'),
-    re_path(r'^logout$', views.logout, name='logout'),
-]'''
