c8898823c99dc52445c9e8bfd03aa9b378fdfe79
fb0801 <fbhatti0801@gmail.com>
2024-05-31 18:30:08 +0100

    Finally, Moving Down to the Models Layer

--- myworld/lists/models.py
+++ myworld/lists/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.conf import settings
 
-
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
@@ -13,8 +12,10 @@ class List(models.Model):
     def name(self):
         return self.item_set.first().text
     
-    def create_new():
-        pass
+    @staticmethod
+    def create_new(first_item_text, owner=None):
+        list_ = List.objects.create(owner=owner)
+        Item.objects.create(text=first_item_text, list=list_)
 
 
 class Item(models.Model):
--- myworld/lists/tests/test_models.py
+++ myworld/lists/tests/test_models.py
@@ -3,6 +3,7 @@ from lists.models import Item, List
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
+
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -35,7 +36,23 @@ class ListModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)'''
     
-    
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+    def test_create_new_creates_list_and_first_item(self):
+        List.create_new(first_item_text='new item text')
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'new item text')
+        new_list = List.objects.first()
+        self.assertEqual(new_item.list, new_list)
+
+    def test_lists_can_have_owners(self):
+        List(owner=User())  # should not raise
+
+
+    def test_list_owner_is_optional(self):
+        List().full_clean()  # should not raise
 
 
     def test_item_is_related_to_list(self):
@@ -61,6 +78,11 @@ class List(models.Model):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+    @staticmethod
+    def create_new(first_item_text):
+        list_ = List.objects.create()
+        Item.objects.create(text=first_item_text, list=list_)
 
     def test_lists_can_have_owners(self):
         user = User.objects.create(email='a@b.com')
@@ -102,3 +124,9 @@ class List(models.Model):
         Item.objects.create(list=list_, text='first item')
         Item.objects.create(list=list_, text='second item')
         self.assertEqual(list_.name, 'first item')
+
+    def test_create_new_optionally_saves_owner(self):
+        user = User.objects.create()
+        List.create_new(first_item_text='new item text', owner=user)
+        new_list = List.objects.first()
+        self.assertEqual(new_list.owner, user)
