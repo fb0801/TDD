56f530664a22f1658cf8ab832952b9587d8b61c2
fb0801 <fbhatti0801@gmail.com>
2024-10-03 18:37:47 +0100

    chp 23 part 2

--- myworld/functional_tests/base.py
+++ myworld/functional_tests/base.py
@@ -5,8 +5,10 @@ from selenium.common.exceptions import WebDriverException
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from pathlib import Path
+from datetime import datetime
 
-
+SCREEN_DUMP_LOCATION = Path(__file__).absolute().parent / "screendumps"
 MAX_WAIT = 10
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -18,7 +20,17 @@ class FunctionalTest(StaticLiveServerTestCase):
             self.live_server_url = 'http://' + staging_server 
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not SCREEN_DUMP_LOCATION.exists():
+                SCREEN_DUMP_LOCATION.mkdir(parents=True)
+            self.take_screenshot()
+            self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+    def _test_has_failed(self):
+        # slightly obscure but couldn't find a better way!
+        return self._outcome.result.failures or self._outcome.result.errors
 
     def wait(fn):
         def modified_fn(*args, **kwargs):  
@@ -80,3 +92,20 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
+
+
+    def take_screenshot(self):
+        path = SCREEN_DUMP_LOCATION / self._get_filename("png")
+        print("screenshotting to", path)
+        self.browser.get_screenshot_as_file(str(path))
+
+    def dump_html(self):
+        path = SCREEN_DUMP_LOCATION / self._get_filename("html")
+        print("dumping page HTML to", path)
+        path.write_text(self.browser.page_source)
+
+    def _get_filename(self, extension):
+        timestamp = datetime.now().isoformat().replace(":", ".")[:19]
+        return (
+            f"{self.__class__.__name__}.{self._testMethodName}-{timestamp}.{extension}"
+        )
